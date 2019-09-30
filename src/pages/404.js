0f930dbdc2fc92fe59f77e7da928477638412589
Daniel Cooper <danielcooper@Daniels-MacBook-Air.local>
2019-09-30 08:07:53 +0700

    creates new branch for testing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 //Modules
 import React from "react"
+import { Link } from "gatsby"
 //Components
 import Layout from "../components/Layout/Layout"
 
@@ -7,6 +8,7 @@ const NotFoundPage = () => (
   <Layout>
     <h1> PAGE NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist...</p>
+    <Link to="/">Head Home</Link>
   </Layout>
 )
 
