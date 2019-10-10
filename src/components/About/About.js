cbab2e1edf230a89c90c2153d1c44d175dd7733b
Daniel Cooper <danielcooper@Daniels-MacBook-Air.local>
2019-10-10 09:35:29 +0700

    adds some copy to about us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,33 @@
 //Modules
 import React from "react"
+import { Link } from "gatsby"
 //Sass
 import styles from "./About.module.scss"
 //Components
 import Profiles from "./Profiles/Profiles"
 import Story from "./Story/Story"
+import Title from "../Utility/Title/Title"
 
 const About = () => {
   return (
     <section className={styles.About}>
       <Story />
+      <Title title="Team" />
       <Profiles />
+      <Title title="Volunteers" />
+      <div className={styles.Volunteers}>
+        <p>
+          We are deeply grateful for our dedicated an passionate volunteers, who
+          are central to facilitating the smooth running of all aspects of the
+          event. Thank you from the bottom of our hearts. We love you guys!
+        </p>
+
+        <p>
+          If you would like to join the Spirit Team and be a volunteer at any of
+          our fairs please <Link tp="/get-involved">apply here</Link> or contact
+          us for more information.
+        </p>
+      </div>
     </section>
   )
 }
