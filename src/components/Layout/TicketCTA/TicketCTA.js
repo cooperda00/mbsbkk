c0e9458290a42a571d63d937c0c1ae540b81f836
Daniel Cooper <danielcooper@Daniels-MacBook-Air.local>
2019-10-24 10:25:40 +0700

    conditionaly checks for window

--- src/components/Layout/TicketCTA/TicketCTA.js
+++ src/components/Layout/TicketCTA/TicketCTA.js
@@ -5,19 +5,21 @@ import styles from "./TicketCTA.module.scss"
 
 const TicketCTA = () => {
   //Dont show on individual blog view (less ugly)
-  if (window && !window.location.href.includes("/blog/")) {
-    return (
-      <a
-        href="https://www.ticketmelon.com"
-        className={styles.TicketCTA}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <p>Buy Tickets</p>
-      </a>
-    )
+  if (typeof window !== undefined) {
+    if (!window.location.href.includes("/blog/")) {
+      return (
+        <a
+          href="https://www.ticketmelon.com"
+          className={styles.TicketCTA}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>Buy Tickets</p>
+        </a>
+      )
+    }
   } else {
-    return null
+    return <div></div>
   }
 }
 
