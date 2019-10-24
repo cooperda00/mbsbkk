b9c52d53c83a652f7d5bbad73639f9707b396a40
Daniel Cooper <danielcooper@Daniels-MacBook-Air.local>
2019-10-24 10:38:07 +0700

    cancels trying to use window object

--- src/components/Layout/TicketCTA/TicketCTA.js
+++ src/components/Layout/TicketCTA/TicketCTA.js
@@ -1,29 +1,19 @@
 //Modules
-import React, { useEffect } from "react"
+import React from "react"
 //Sass
 import styles from "./TicketCTA.module.scss"
 
 const TicketCTA = () => {
-  //Dont show on individual blog view (less ugly)
-  useEffect(() => {
-    if (
-      typeof window !== undefined &&
-      !window.location.href.includes("/blog/")
-    ) {
-      return (
-        <a
-          href="https://www.ticketmelon.com"
-          className={styles.TicketCTA}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>Buy Tickets</p>
-        </a>
-      )
-    } else {
-      return null
-    }
-  }, [])
+  return (
+    <a
+      href="https://www.ticketmelon.com"
+      className={styles.TicketCTA}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <p>Buy Tickets</p>
+    </a>
+  )
 }
 
 export default TicketCTA
